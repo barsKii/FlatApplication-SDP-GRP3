8fe7685568b8c41aeb6be7b6039d3df48f65110d
iAmacone <hahaahaaah@hotmail.com>
2016-09-07 00:11:57 +1200

    Welcome completed Welcome loads for 5s(can be changed), switching straight into menu

--- app/src/main/java/com/sdpcrew/android/flatapp/MainActivity.java
+++ app/src/main/java/com/sdpcrew/android/flatapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sdpcrew.android.flatapp;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,22 +13,27 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mTasksButton;
     private ImageButton mBillsButton;
 
+    private Handler mControlTimer = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.welcome);
 
         // All buttons initialized to the linked button, no listener as of yet. Coolies
         mCalendarButton = (ImageButton) findViewById(R.id.calendar_button);
         mTasksButton = (ImageButton) findViewById(R.id.tasks_button);
         mBillsButton = (ImageButton) findViewById(R.id.bills_button);
 
-
-
-
+        mControlTimer.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_main);
+            }
+        },5000);
 
     }
+
 
     // Shane's code starts here. Please don't touch :)
     public void showShoppingListClick(View v) {
