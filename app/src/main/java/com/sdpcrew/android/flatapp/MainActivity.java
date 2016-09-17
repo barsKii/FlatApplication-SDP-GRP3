cf0ad4b8e709cafc7b2ea606722f7e02aca54013
iAmacone <hahaahaaah@hotmail.com>
2016-09-18 01:19:41 +1200

    Calendar ui updated Calendar day ui added

--- app/src/main/java/com/sdpcrew/android/flatapp/MainActivity.java
+++ app/src/main/java/com/sdpcrew/android/flatapp/MainActivity.java
@@ -5,17 +5,10 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-
 import com.sdpcrew.android.flatapp.TasksManager.TaskManagerActivity;
-
 import com.sdpcrew.android.flatapp.Calender.CalendarMain;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ImageButton mCalendarButton;
-    private ImageButton mTasksButton;
-    private ImageButton mBillsButton;
 
     private Handler mControlTimer = new Handler();
 
@@ -24,20 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
 
-        // All buttons initialized to the linked button, no listener as of yet. Coolies
-        mCalendarButton = (ImageButton) findViewById(R.id.calendar_button);
-        mTasksButton = (ImageButton) findViewById(R.id.tasks_button);
-        mBillsButton = (ImageButton) findViewById(R.id.bills_button);
-
         mControlTimer.postDelayed(new Runnable() {
             @Override
             public void run() {
                 setContentView(R.layout.activity_main);
             }
-        },3000);
+        },1000);
 
     }
-
 
     // Shane's code starts here. Please don't touch :)
     public void showShoppingListClick(View v) {
@@ -48,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     public void showTaskManagerClick(View v) {
         startActivity(new Intent(this, TaskManagerActivity.class));
     }
+
     public void showCalendar (View v) {
         startActivity(new Intent(this, CalendarMain.class));
     }
