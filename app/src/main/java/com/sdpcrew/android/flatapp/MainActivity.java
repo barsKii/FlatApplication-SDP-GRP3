1bc7b1cc99ab4f3abdafa6f47ee76e07d2a0da1a
iAmacone <hahaahaaah@hotmail.com>
2016-09-22 11:51:03 +1200

    Calendar next/prev bug fixed Calendar flow bug fixed

--- app/src/main/java/com/sdpcrew/android/flatapp/MainActivity.java
+++ app/src/main/java/com/sdpcrew/android/flatapp/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 setContentView(R.layout.activity_main);
             }
-        },1000);
+        },2000);
 
     }
 
