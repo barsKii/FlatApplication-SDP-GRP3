a9e5804b27a3c2a51915784273c6446371011443
vinivaf <vini_vaf@hotmail.com>
2016-10-06 11:45:54 +1300

    Comented class DialogQualifier

--- app/src/main/java/com/sdpcrew/android/flatapp/Splash.java
+++ app/src/main/java/com/sdpcrew/android/flatapp/Splash.java
@@ -5,20 +5,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+/**
+ * This class is responsible for the welcome screen ( splashScreen) or the initilization screen
+ * It will show a picture while the app loads. We plan to implement a loading bar too
+ */
 public class Splash extends Activity {
-    private final int SPLASH_DISPLAY_LENGTH = 2000;
-    private static final String STARTED = "started";
-    private static boolean started = false;
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000; //Sets time splash screen will be active
+    private static final String STARTED = "started"; // for saving purpose when app is destroyed
+    private boolean started = false; //holds info whether this screen was shown or not already
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //retrieves value store in STARTED constant and updates started variable
         if (savedInstanceState != null) {
             started = savedInstanceState.getBoolean(STARTED);
         }
-        //This is not working at the moment
-        if(!started){
+        //This is not working the way I intended at the moment
+        //If app has been initialzed creates an intent of main class and ends this activity.
+        // Otherwise, create an intent and only finish it when the SPLASH_DISPLAY_LENGTH ends.
+        if(started){
             Intent mainIntent = new Intent(Splash.this,MainActivity.class);
             Splash.this.startActivity(mainIntent);
             Splash.this.finish();
@@ -29,7 +37,7 @@ public class Splash extends Activity {
                 public void run() {
                     Intent mainIntent = new Intent(Splash.this,MainActivity.class);
                     Splash.this.startActivity(mainIntent);
-                    Splash.this.finish();
+                    Splash.this.finish();//finishes this activity
 
                 }
             }, SPLASH_DISPLAY_LENGTH);
--- app/src/main/java/com/sdpcrew/android/flatapp/TasksManager/DialogQualifier.java
+++ app/src/main/java/com/sdpcrew/android/flatapp/TasksManager/DialogQualifier.java
@@ -16,15 +16,24 @@ import android.widget.Toast;
 
 import com.sdpcrew.android.flatapp.R;
 
+/**
+ * This is a DialogFragment used as a pop dialog which holds for now
+ * a title field only. It holds a method newInstance were stores an argument. It also
+ * send information back to the caller class when info is updated.
+ * This fragment has two functions, either create a new Qualifier or alter a qualifier.
+ * Qualifier is a list which holds several tasks
+ */
 public class DialogQualifier extends DialogFragment {
 
+    //Variables
     private EditText mTitle;
     private String mNewTitle;
     private Qualifier mNewQualifier;
-
+    //Constants
     private static final String ARG_QUALIFIER = "QualifierName";
     private static final String EXTRA_RETURN_DATA = "com.sdpcrew.android.flatapp.TasksManager.DialogQualifier";
 
+    //return this fragment at sametime stores an argument to this
     public static DialogQualifier newInstance(String qualifier ) {
         Bundle args = new Bundle();
         args.putSerializable(ARG_QUALIFIER, qualifier);
@@ -35,7 +44,7 @@ public class DialogQualifier extends DialogFragment {
 
     public Dialog onCreateDialog(Bundle savedInstanceState){
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_dialog_qualifier, null);
-
+        //controls editable text fild (Title)
         mTitle = (EditText) v.findViewById(R.id.dialog_qualifier_settitle);
         mTitle.addTextChangedListener(new TextWatcher() {
             @Override
@@ -51,6 +60,7 @@ public class DialogQualifier extends DialogFragment {
             public void afterTextChanged(Editable editable) {
             }
         });
+        //Gets argument stored, it will check for a existent qulifier with same name as the argument
         if(getArguments() != null) {
             String qualifierTitle = (String) getArguments().getSerializable(ARG_QUALIFIER);
             if (QualifierLab.get(getContext()).getQualifier(qualifierTitle) != null) {
@@ -59,12 +69,15 @@ public class DialogQualifier extends DialogFragment {
             }
         }
 
+        //Return a Alert diaglog with an OK button which will determine if new list should be created or updated
         return new AlertDialog.Builder(getActivity()).setView(v)
                 .setPositiveButton(android.R.string.ok,
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+                                //if there is no qualifier creates one
                                 if (mNewQualifier == null) {
+                                    //if title is null or too short no object is created, mits a toast
                                     if (mTitle == null || mTitle.length() <= 2) {
                                         Toast.makeText(getActivity(),
                                                 R.string.no_title_task_enter, Toast.LENGTH_LONG)
@@ -73,11 +86,11 @@ public class DialogQualifier extends DialogFragment {
                                         mNewQualifier = new Qualifier();
                                         mNewQualifier.setTitle(mNewTitle);
                                         QualifierLab.get(getContext()).addQualifier(mNewQualifier);
-                                        sendResult(Activity.RESULT_OK);
+                                        sendResult(Activity.RESULT_OK);//sends result
                                     }
                                 }else{
                                     mNewQualifier.setTitle(mNewTitle);
-                                    sendResult(Activity.RESULT_OK);
+                                    sendResult(Activity.RESULT_OK);//sends result
                                 }
                             }
                         })
@@ -85,6 +98,12 @@ public class DialogQualifier extends DialogFragment {
 
     }
 
+    /**
+     * This method put an extra which should be detected on the caller fragment.
+     * this method manually calls the onActivityResult method of the caller fragment so it can detect
+     * alteration and therefore update view.
+     * @param resultCode
+     */
     private void sendResult(int resultCode) {
         if (getTargetFragment() == null) {
             return;
